227019bd04109c48ffe778ce52f3532f48e70b3c
P5607 <p56071078@gs.ncku.edu.tw>
2021-01-28 13:30:33 +0800

    兩種轉成RGB的方法

--- mpl2.py
+++ mpl2.py
@@ -1,5 +1,6 @@
 # 用matplotlib顯示彩色圖檔，如果不修改會是以BGR顯示出來的圖，照片會錯
 
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,3 +16,20 @@ cv2.imshow('bgr image',img) # expects true color
 cv2.imshow('rgb image',img2) # expects distorted color
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+#-------------------------------------------
+#另一種寫法
+
+import cv2
+#簡寫numpy為np
+import numpy as np
+#匯入matplotlib套件，裏頭有各種繪圖方法
+from matplotlib import pyplot as plt
+# import matplotlib.pyplot as plt
+img = cv2.imread('test.jpg',1)
+img1=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.imshow(img1)
+#顯示xy座標值的顏色
+plt.xticks([]),plt.yticks([])
+plt.show()
